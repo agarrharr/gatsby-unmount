03b264d7a0976336f3cd309d1d85b57de120fc10
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-01-01 10:14:06 -0700

    Add audio player

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,37 +5,51 @@ import { StaticQuery, graphql } from 'gatsby'
 import Header from './header'
 import './layout.css'
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+class Layout extends React.Component {
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+  render() {
+    const { children } = this.props
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
           }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
+        `}
+        render={data => (
+          <>
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <audio controls>
+              <source
+                src="https://s3-us-west-2.amazonaws.com/bookbytes/bb-018.mp3"
+                type="audio/mp3"
+              />
+            </audio>
+            <div
+              style={{
+                margin: `0 auto`,
+                maxWidth: 960,
+                padding: `0px 1.0875rem 1.45rem`,
+                paddingTop: 0,
+              }}
+            >
+              {children}
+              <footer>
+                © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+              </footer>
+            </div>
+          </>
+        )}
+      />
+    )
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
